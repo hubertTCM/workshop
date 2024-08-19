bc43c63c5360a40fe4cb9826014768c49d7be960
Hubert <fuqunxing@gmail.com>
2024-08-20 00:06:58 +1200

    Console App

---
+++ dotnet-workspace/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class EntryClass
+    {
+        static void Main(string[] args)
+        {
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
